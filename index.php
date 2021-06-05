1197d548e98fa4448168719b922752016b4a651c
Rowan <b7012065@my.shu.ac.uk>
2021-06-05 18:25:18 +0100

    moved around the styling

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ require("requires/header.php");
 
 ?>
 <body>
-    <?php require("requires/navbar.php") // insert navbar ?>
     <div class="container">
+        <?php require("requires/navbar.php") // insert navbar ?>
         <a style="margin: 5px; margin-left: 10px;" class="btn btn-primary" href="devices.php"> Devices </a>
     </div>
 </body>
