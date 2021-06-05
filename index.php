35123eae626a6f4080c8803d6f6262ec9181636e
Rowan <b7012065@my.shu.ac.uk>
2021-06-05 18:24:53 +0100

    improved index

--- index.php
+++ index.php
@@ -7,7 +7,9 @@ require("requires/header.php");
 ?>
 <body>
     <?php require("requires/navbar.php") // insert navbar ?>
-    <a style="margin: 5px; margin-left: 10px;" class="btn btn-primary" href="devices.php"> Devices </a>
+    <div class="container">
+        <a style="margin: 5px; margin-left: 10px;" class="btn btn-primary" href="devices.php"> Devices </a>
+    </div>
 </body>
 <?php
 // Include footer
