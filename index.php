15cc7126ce0b1817548c11cb8de95386929c3e28
Rowan <b7012065@my.shu.ac.uk>
2021-06-05 18:22:52 +0100

    added navbar to index

--- index.php
+++ index.php
@@ -5,9 +5,10 @@ $page_name = "Index"; // Set page name
 require("requires/header.php");
 
 ?>
-
+<body>
+    <?php require("requires/navbar.php") // insert navbar ?>
     <a style="margin: 5px; margin-left: 10px;" class="btn btn-primary" href="devices.php"> Devices </a>
-
+</body>
 <?php
 // Include footer
 require("requires/footer.php");
