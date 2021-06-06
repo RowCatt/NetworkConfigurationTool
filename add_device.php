41efbb415997033af994514782a454ba12962d6e
Rowan <b7012065@my.shu.ac.uk>
2021-06-06 11:16:57 +0100

    add device form

---
+++ add_device.php
@@ -0,0 +1,65 @@
+<?php
+
+$page_name = "Devices"; // Set page name
+// Insert header
+require("requires/header.php");
+
+// Add databse connection info
+require("requires/connect.php");
+
+?>
+
+<body>
+
+    <div class="container">
+
+        <?php
+
+        require("requires/navbar.php") // insert navbar
+
+        ?>
+
+        <div class='container'>
+
+            <h3> Add Device </h3>
+
+            <form action='process/addDeviceProcess.php' method='post'>
+
+                <label for="ip">IP Address</label>
+                <input type="text" name="ip">
+
+                <label for="username">Username</label>
+                <input type="text" name="username">
+
+                <label for="password">Password</label>
+                <input type="text" name="password">
+
+                <label for="model">Model</label>
+                <select name="model">
+                    <?php
+
+                    // Get all models from DB
+                    $query = "SELECT * FROM `models`";
+                    $query = mysqli_query($connect,$query);
+                    while($model = mysqli_fetch_assoc($query))
+                    {
+                        $model = $model["name"];
+                        echo "<option value='$model'>$model</option>";
+                    }
+
+                    ?>
+                </select>
+
+                <input type="submit" value="Save">
+
+            </form>
+
+        </div>
+
+    </div>
+</body>
+
+<?php
+// Include footer
+require("requires/footer.php");
+?>
--- devices.php
+++ devices.php
@@ -10,44 +10,45 @@ require("requires/connect.php");
 ?>
 
 <body>
-    <?php require("requires/navbar.php") // insert navbar ?>
-</body>
 
-<div class="container">
+    <div class="container">
 
-    <?php
+        <?php
 
-    // Add Device Page link
-    echo "<a style='margin: 5px; margin-left: 10px;' class='btn btn-primary' href='add_device.php'> Add Device </a>";
+        require("requires/navbar.php") // insert navbar
 
-    // List devices in the devices table
-    $query = "SELECT * FROM `devices`";
-    $query = mysqli_query($connect,$query);
+        // Add Device Page link
+        echo "<a style='margin: 5px; margin-left: 10px;' class='btn btn-primary' href='add_device.php'> Add Device </a>";
 
-    // Check to see if there are any entries in the devices table
-    if(mysqli_num_rows($query) == 0) // If there's no entries
-    {  
-        echo "<p> No Devices in the Database. </p>";
-    }else
-    {
+        // List devices in the devices table
+        $query = "SELECT * FROM `devices`";
+        $query = mysqli_query($connect,$query);
 
-        // Display all devices
-        while($currentDevice = mysqli_fetch_assoc($query))
+        // Check to see if there are any entries in the devices table
+        if(mysqli_num_rows($query) == 0) // If there's no entries
+        {  
+            echo "<p> No Devices in the Database. </p>";
+        }else
         {
-            $ip_address = $currentDevice["ip_address"];
-            $model = $currentDevice["model"];
-            // Get model from the models table
-            $query_model = "SELECT * FROM `models` WHERE `id`=`$model`";
-            $query_model = mysqli_query($connect,$query_model);
-            $model = $query_model["name"];
 
-            echo "<p> $ip_address - $model</p>";
+            // Display all devices
+            while($currentDevice = mysqli_fetch_assoc($query))
+            {
+                $ip_address = $currentDevice["ip_address"];
+                $model = $currentDevice["model"];
+                // Get model from the models table
+                $query_model = "SELECT * FROM `models` WHERE `id`=`$model`";
+                $query_model = mysqli_query($connect,$query_model);
+                $model = $query_model["name"];
+
+                echo "<p> $ip_address - $model</p>";
+            }
         }
-    }
 
-    ?>
+        ?>
 
-</div>
+    </div>
+</body>
 
 <?php
 // Include footer
