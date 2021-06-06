08e76e666021c69a481555b6dd5f5002ad5aaa57
Rowan <b7012065@my.shu.ac.uk>
2021-06-06 20:55:03 +0100

    boostrapped it

--- devices.php
+++ devices.php
@@ -18,9 +18,9 @@ require("requires/connect.php");
         require("requires/navbar.php"); // insert navbar
 
         // Add Device Page link
-        echo "<a style='margin: 5px; margin-left: 10px;' class='btn btn-primary' href='add_device.php'> Add Device </a>";
+        echo "<a style='margin: 15px;' class='btn btn-primary' href='add_device.php'> Add New Device </a>";
 
-        // List devices in the devices table
+        // Get devices in the devices table
         $query = "SELECT * FROM `devices`";
         $query = mysqli_query($connect,$query);
 
@@ -31,20 +31,40 @@ require("requires/connect.php");
         }else
         {
 
-            // Display all devices
-            while($currentDevice = mysqli_fetch_assoc($query))
-            {
-                $ip_address = $currentDevice["ip_address"];
-                $model = $currentDevice["model"];
-                // Get model from the models table
-                $query_model = "SELECT * FROM `models` WHERE `id`='$model'";
-                $query_model = mysqli_query($connect,$query_model);
-                while($fetched_model = mysqli_fetch_assoc($query_model)){
-                    $model = $fetched_model["name"];
-                }
-                
-                echo "<p> $ip_address - $model</p>";
-            }
+            // Devices table
+            echo "<table class='table'>";
+                echo "<thead>";
+                    echo "<tr>";
+                        echo "<th scope='col'> IP Address </th>";
+                        echo "<th scope='col'> Model </th>";
+                        echo "<th scope='col'> Action </th>";
+                    echo "</tr>";
+                echo "</thead>";
+                echo "<tbody>";
+
+                        // Display all devices
+                        while($currentDevice = mysqli_fetch_assoc($query))
+                        {
+                            $id = $currentDevice["id"];
+                            $ip_address = $currentDevice["ip_address"];
+                            $model = $currentDevice["model"];
+                            // Get model from the models table
+                            $query_model = "SELECT * FROM `models` WHERE `id`='$model'";
+                            $query_model = mysqli_query($connect,$query_model);
+                            while($fetched_model = mysqli_fetch_assoc($query_model)){
+                                $model = $fetched_model["name"];
+                            }
+
+                            echo "<tr>";
+                                echo "<td> $ip_address </td>";
+                                echo "<td> $model </td>";
+                                echo "<td> <a style='margin: 15px;' class='btn btn-primary' href='edit_device_config.php?id=$id'> Edit Configuration </a> </td>";
+                            echo "</tr>";
+                            
+                        }
+
+                echo "</tbody>";
+            echo "</table>";
         }
 
         ?>
