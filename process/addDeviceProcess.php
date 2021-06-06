8e2844266757ffc4506cef6737e74bacfde4b49f
Rowan <b7012065@my.shu.ac.uk>
2021-06-06 12:03:50 +0100

    error checking

--- process/addDeviceProcess.php
+++ process/addDeviceProcess.php
@@ -2,12 +2,15 @@
 
 require("../requires/connect.php");
 
-// Check if data has been entered
+// ERROR CHECKING
 
-if(isset($_POST["ip"]) && $_POST["ip"] != ""){
+if(isset($_POST["ip"]) && $_POST["ip"] != "" && isset($_POST["username"]) && $_POST["username"] != "" && isset($_POST["password"]) && $_POST["password"] != "" && isset($_POST["model"]) && $_POST["model"] != ""){
     $ip = $_POST["ip"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $model = $_POST["model"];
 }else{
-    header("Location: ../add_device.php?error=IP Address field empty");
+    header("Location: ../add_device.php?error=Fields left empty");
     exit;
 }
 
