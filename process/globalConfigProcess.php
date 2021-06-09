e061fb790a17b71d82405732239200c507361d4e
Rowan <b7012065@my.shu.ac.uk>
2021-06-09 18:02:53 +0100

    global config

--- process/globalConfigProcess.php
+++ process/globalConfigProcess.php
@@ -9,14 +9,20 @@ if(isset($_POST["username"]) && $_POST["username"] != "" && isset($_POST["passwo
     $password = $_POST["password"];
     $domain_name = $_POST["domain_name"];
 }else{
-    header("Location: ../devices.php");
+    header("Location: ../global_config.php?error=Fields left empty.");
     exit;
 }
 
 if(!filter_var($domain_name, FILTER_VALIDATE_DOMAIN)){
-    echo "not a domain";
-}else{
-    echo "is a domain";
+    // Is not a domain, send back with error
+    header("Location: ../global_config.php?error=Domain name not valid.");
+    exit;
 }
+
+// Update the global config table
+$query = "UPDATE global_configuration SET username='$username', password='$password', `domain-name`='$domain_name' WHERE id='1'";
+$query = mysqli_query($connect,$query);
+
+header("Location: ../global_config.php");
 
 ?>
