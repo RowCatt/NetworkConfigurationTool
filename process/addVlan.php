1640e23f4bd8d95a48882978ae0aced3ae452642
Rowan <b7012065@my.shu.ac.uk>
2021-06-15 17:42:46 +0100

    wrong var

--- process/addVlan.php
+++ process/addVlan.php
@@ -5,13 +5,13 @@ if(isset($_POST["id"]) && $_POST["id"] != "" && isset($_POST["name"]) && $_POST[
     $id = $_POST["id"];
     $name = $_POST["name"];
 }else{
-    header("Location: ../vlanMangement.php?error=Fields left empty.");
+    header("Location: ../vlanManagement.php.php?error=Fields left empty.");
     exit;
 }
 
 // Error if id is 1
 if($id == 1){
-    header("Location: ../vlanMangement.php?error=VLAN Number already in use.");
+    header("Location: ../vlanManagement.php.php?error=VLAN Number already in use.");
     exit;
 }
 
@@ -20,13 +20,13 @@ $query = "SELECT * FROM vlans WHERE vlan_number='$id'";
 $query = mysqli_query($connect,$query);
 if(mysqli_num_rows($query) > 0){
     // VLAN Number already in use
-    header("Location: ../vlanMangement.php?error=VLAN Number already in use.");
+    header("Location: ../vlanManagement.php.php?error=VLAN Number already in use.");
     exit;                                                                                                                                                                                                                                                                                                                                        
 }
 $query = "SELECT * FROM vlans WHERE vlan_name='$name'";
 $query = mysqli_query($connect,$query);
 if(mysqli_num_rows($query) > 0){
-    header("Location: ../vlanMangement.php?error=VLAN Name already in use.");
+    header("Location: ../vlanManagement.php.php?error=VLAN Name already in use.");
     exit;                                                                                                                                                                                                                                                                                                                                        
 }
 
@@ -34,6 +34,6 @@ if(mysqli_num_rows($query) > 0){
 $query = "INSERT INTO vlans (vlan_number, vlan_name) VALUES ('$id', '$name')";
 mysqli_query($connect,$query);
 
-header("Location: ../vlanMangement.php");
+header("Location: ../vlanManagement.php.php");
 
 ?>
