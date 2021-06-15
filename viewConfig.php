6156fe42c11640a4198217b9d2fff68e1a249fb3
Rowan <b7012065@my.shu.ac.uk>
2021-06-15 14:31:23 +0100

    view config page

---
+++ viewConfig.php
@@ -0,0 +1,12 @@
+<?php
+require("requires/connect.php");
+$config_id = $_GET["id"];
+$query = "SELECT * FROM configurations WHERE id='$config_id'";
+$query = mysqli_query($connect,$query);
+while($config = mysqli_fetch_assoc($query)){
+    $running_config = $config["configuration"];
+}
+
+echo "<pre> $running_config </pre>";
+
+?>
